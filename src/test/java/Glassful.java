bcfa46ea376cb7c75548b60df64ef50ce2d38763
qwer083 <Qwertyiop890>
2022-10-30 20:08:40 +0300

    final

--- src/test/java/Glassful.java
+++ src/test/java/Glassful.java
@@ -1,9 +1,10 @@
 public class Glassful {
     public static void main(String []args) {
-        int a = 350;
+        final int a = 350;
         double b = 0.45;
         double c = a*b;
-        String s = "объём заполненного стакана";
+        System.out.println("объём стакана в милилитрах: " + a);
+        System.out.println("заполненность стакана: " + b);
         System.out.println("объём заполненного стакана: " + c);
     }
 }
