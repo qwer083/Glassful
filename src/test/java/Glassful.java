a7a5d0faab829dc3c21b7f550e2b2f6ba9b43e23
qwer083 <Qwertyiop890>
2022-10-30 18:19:03 +0300

    Initial commit

---
+++ src/test/java/Glassful.java
@@ -0,0 +1,9 @@
+public class Glassful {
+    public static void main(String []args) {
+        int a = 350;
+        double b = 0.45;
+        double c = a*b;
+        String s = "объём заполненного стакана";
+        System.out.println("объём заполненного стакана: " + c);
+    }
+}
